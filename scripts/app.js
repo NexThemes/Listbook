d228d888d47d90a531704e82a73fa7fbc9e1d993
Valentin Scarevnea <vscarevnea@gmail.com>
2017-02-15 02:24:28 +0200

    02 noaptea

---
+++ scripts/app.js
@@ -0,0 +1,159 @@
+// start angular Spa App
+var app = angular.module('spaApp', ["ngRoute"]);
+
+// factory of getting data from json file
+app.factory("jsonFactory", function($http) {
+  function getData() {
+    return $http({method:'GET',url:'database/AgendaDataSource.json'});
+  }
+  return {
+    getData: getData
+  }
+});
+
+// User Details controller
+app.controller("spaUserDetailsCtrl", function($scope, $routeParams, jsonFactory) {
+    // getting data from JSON file
+    jsonFactory.getData().then(function(response){
+        $scope.users = response.data;
+    });
+    $scope.title = "User Details";
+    $scope.pageId = $routeParams.id;
+});
+
+// Controller loads data from JSON file
+app.controller("spaJSONCtrl", function($scope, jsonFactory) {
+    // getting data from JSON file
+    jsonFactory.getData().then(function(response){
+      var users = response.data;
+      $scope.delete = function(id) {
+        for(user in users) {
+          if(users[user].id == id) {
+            users.splice(user, 1);
+          }
+        }
+      }
+      $scope.users = users;
+    });
+
+    $scope.delete = function(id) {
+      for(user in users) {
+        if(users[user].id == id) {
+          users.splice(user, 1);
+          localStorage.setItem('users', angular.toJson(users));
+        }
+      }
+    }
+
+    // sort by column
+    $scope.sortColumn = "name";
+    $scope.reverseSort = false;
+    $scope.sortData = function(column){
+        $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+        $scope.sortColumn = column;
+    };
+    // adding or removing arrows class
+    $scope.getSortClass = function(column) {
+        if($scope.sortColumn == column) {
+            return $scope.reverseSort ? 'arrow-down' : 'arrow-up'
+        };
+        return '';
+    };
+});
+
+// geting data from localstorage service
+app.service('usersChanges', function() {
+  var users = angular.fromJson(localStorage.getItem('users')) || [];
+  var getUsers = function() {
+    return users;
+  };
+  var addUser = function(user) {
+    users.push(user);
+    localStorage.setItem('users', angular.toJson(users));
+  };
+  var removeUser = function(id) {
+    for(user in users) {
+      if(users[user].id == id) {
+        users.splice(user, 1);
+        localStorage.setItem('users', angular.toJson(users));
+      }
+    }
+  };
+  return {
+    addUser  : addUser,
+    getUsers : getUsers,
+    removeUser : removeUser
+  };
+});
+
+// Save user controller
+app.controller("spaNewUserCtrl", function($scope, $location, usersChanges) {
+    $scope.title = "Register User";
+    $scope.saveUser = function(user) {
+      var user = {
+          "id": (new Date()).getTime(),
+          "name": user.name,
+          "surname": user.surname,
+          "age": user.age,
+          "born": user.born,
+          "phone": {"home":user.phone.home,"work":user.phone.work},
+          "address":{"home":user.address.home,"work":user.address.work},
+          "note":user.note,
+          "created": new Date()
+      };
+      usersChanges.addUser(user);
+      $location.path('/');
+    };
+});
+
+// Edit user controller
+app.controller("editUserCtrl", function($scope, $location, $routeParams) {
+    $scope.title = "Edit User";
+    $scope.pageId = $routeParams.id;
+    $scope.editing = true;
+    //jsonFactory.getData().then(function(response){
+    var users = angular.fromJson(localStorage.getItem('users')) || [];
+      for(user in users) {
+        if(users[user].id == $routeParams.id) {
+          $scope.user = users[user];
+        }
+      }
+    $scope.editUser = function(user) {
+      users.splice(user, 1);
+      users.push(user);
+      localStorage.setItem('users', angular.toJson(users));
+      $location.path('/');
+    }
+    //});
+});
+
+// Controller loads data from localStorage
+app.controller("spaStorageCtrl", function($scope, usersChanges) {
+    // getting data from localStorage
+    var users = usersChanges.getUsers();
+    $scope.users = users;
+    $scope.delete = function(id) { usersChanges.removeUser(id) };
+
+    // sort by column
+    $scope.sortColumn = "name";
+    $scope.reverseSort = false;
+    $scope.sortData = function(column){
+        $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+        $scope.sortColumn = column;
+    };
+    // adding or removing arrows class
+    $scope.getSortClass = function(column) {
+        if($scope.sortColumn == column) {
+            return $scope.reverseSort ? 'arrow-down' : 'arrow-up'
+        };
+        return '';
+    };
+});
+
+// User Details controller LocalStorage
+app.controller("spaUsersDetailsCtrl", function($scope, $http, $routeParams) {
+    // getting data from JSON file
+    $scope.users = angular.fromJson(localStorage.getItem('users'));
+    $scope.title = "User Details";
+    $scope.pageId = $routeParams.id;
+});
---
+++ scripts/routes.js
@@ -0,0 +1,27 @@
+// Routing Configuration
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "views/home.html"
+        })
+        .when("/detail/:id", {
+            templateUrl : "views/userDetail.html",
+            controller : "spaUserDetailsCtrl"
+        })
+        .when("/details/:id", {
+            templateUrl : "views/userDetail.html",
+            controller : "spaUsersDetailsCtrl"
+        })
+        .when("/addnew", {
+            templateUrl : "views/registerUser.html",
+            controller : "spaNewUserCtrl"
+        })
+        .when("/edit/:id", {
+            templateUrl : "views/registerUser.html",
+            controller : "editUserCtrl"
+        })
+        .otherwise({
+            redirectTo: "/"
+        });
+    $locationProvider.html5Mode(true);
+});
